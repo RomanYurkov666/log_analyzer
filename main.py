c008c01ee2a9f10eb7cee3302d600fde0e4f4796
Roman Yurkov <ryurkov@localhost.localdomain>
2020-05-10 14:57:17 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,133 @@
+import os
+import re
+import gzip
+import collections
+import datetime
+import logging
+
+logger = logging.getLogger('DefaultLogger')
+config = {
+    'REPORT_SIZE': 1000,
+    'REPORT_DIR': 'resources/REPORTS_DIR/',
+    'REPORT_SAMPLE': 'resources/REPORT_SAMPLE/report.html',
+    'LOG_DIR': 'resources/LOG_DIR/',
+    'MAX_DROP': 5
+}
+
+LogFile = collections.namedtuple('LogFile', ['name', 'date'])
+
+
+def logger_setup(config_file):
+    """logger initialization function.
+    Args:
+        config_file (dict): dict with parameters for logger.
+    Returns:
+        logger instance.
+    """
+    file = config_file.get('LOG_FILENAME')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(filename=file, format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    return logger
+
+
+def parse_log(file):
+    """function of parsing the specified nginx access log file.
+    Args:
+        file (str): parsing file path.
+    Returns:
+        data: tuple with ip,date,urls.
+        summary_lines: count of lines in file
+    """
+    line_format = re.compile(r'(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<dateandtime>\d{2}\/[a-zA-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2})\ .* (?P<url>[\"][http://].*/[\"])', re.IGNORECASE)
+    logger.info(f'starting to parse the file {file}')
+    opener = gzip.open if file.endswith('.gz') else open
+    with opener(file, 'r') as f:
+        parsed_lines = 0
+        summary_lines = 0
+        for line in f:
+            summary_lines += 1
+            data = re.findall(line_format, line)
+            if data:
+                parsed_lines += 1
+                yield data, summary_lines, parsed_lines
+
+def analyze_parsed_log(log_parser,Top):
+    """function for analyzing parsed data.
+    Args:
+        log_parser (dict): dict with parsed data.
+        top (int): number of most common values
+    Returns:
+        1) Top IPs by hits
+        2) Top URLs by hits
+        3) Total hits per month sorted by month
+        4) Unique visits (by ip) per month sorted by month
+        5) Top IPs barchart per month
+    """
+    ip_counter = collections.Counter()
+    url_counter = collections.Counter()
+    data_counter = collections.Counter()
+    ip = collections.defaultdict(list)
+    for data, summary_lines, parsed_lines in log_parser:
+        for i in data:
+            dm = datetime.datetime.strptime(i[1], '%d/%b/%Y:%H:%M:%S').date().strftime('%b %Y')
+            ip_counter[i[0]] += 1
+            url_counter[i[2]] += 1
+            data_counter[dm] += 1
+            ip[dm].append(i[0])
+    dropped = round((summary_lines - parsed_lines) / summary_lines * 100, 3)
+    print(f'Sun lines: {summary_lines} Pased lines: {parsed_lines} Dropped: {dropped}%')
+    print(f'Top {top} IP Addresses by hits')
+    for k,v in ip_counter.most_common(top):
+        print('{k:<{k_width}}{between}{v:<{v_width}}'.format(
+            k=k, k_width=len(k),
+            between=' ' * (3 + (15 - len(k))),
+            v=v, v_width=len(str(v)) ))
+    c = collections.Counter()
+    for k,v in ip.items():
+        #print(k,v)
+        for i in v:
+            c[i] += 1
+    print(c)
+        #print(collections.Counter(ip[v]))
+    #     c=collections.Counter(ip[k])
+    #     for i in c.items():
+    #         print(i)
+    #         ip_counter=collections.Counter(c.items())
+    #     print(c)
+    # print(ip_counter)
+    print()
+    print(f'Top {top} URLs by hits')
+    for k,v in url_counter.most_common(top):
+        print('{v:<{v_width}}{between}{k:<{k_width}}'.format(
+            k=k, k_width=len(k),
+            between=' ' * (3 + (5 - len(str(v)))),
+            v=v, v_width=len(str(v))))
+    print()
+    for k,v in sorted(data_counter.items(), key = lambda pair: datetime.datetime.strptime(pair[0],'%b %Y').timestamp(), reverse = True):
+        print(f'{k} hits count: {v}')
+    print()
+    for k,v in sorted(ip.items(), key = lambda pair: datetime.datetime.strptime(pair[0],'%b %Y').timestamp(), reverse = True):
+        print(f'{k} unique visits: {len(set(v))}')
+    print()
+    print(f'Top {top} IPs by month')
+    for k,v in ip.items():
+        print(k)
+        print('Total Hits      Ip Address       Graph')
+        for i,j in collections.Counter(ip[k]).most_common(top):
+            print('{j:<{j_width}}    {i:>{i_width}}{between}{c:<{c_width}}'.format(
+                j=j , j_width=len('Total hits'),
+                i=i , i_width=len('IP Address'), between=' '*(3+(15-len(i))),
+                c='#' * int(1000*round(j/(len(ip[k])),4)), c_width=len('Graph') ))
+
+if __name__ == '__main__':
+    file='access.log'
+    top = 10
+    log_parser = parse_log(file)
+    urls = analyze_parsed_log(log_parser,top)
+
+
+
+
+
+
